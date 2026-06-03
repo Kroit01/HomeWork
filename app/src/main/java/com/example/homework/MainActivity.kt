2104648fa9ea83a15f3598cdb5e208337116eb25
Kroit <alex.panaetov.96@gmail.com>
2026-06-03 19:30:14 +0400

    reset main to default

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -10,27 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val searchButton = findViewById<Button>(R.id.search1)
-        val libraryButton = findViewById<Button>(R.id.library)
-        val settingsButton = findViewById<Button>(R.id.setting1)
 
-
-        // Переход на экран Поиска
-        searchButton.setOnClickListener {
-            val displayIntent = Intent(this, Search::class.java)
-            startActivity(displayIntent)
-        }
-
-        // Переход на экран Медиатеки
-        libraryButton.setOnClickListener {
-            val displayIntent = Intent(this, Library::class.java)
-            startActivity(displayIntent)
-        }
-
-// Переход на экран Настроек
-        settingsButton.setOnClickListener {
-            val displayIntent = Intent(this, Settings::class.java)
-            startActivity(displayIntent)
         }
     }
-}
