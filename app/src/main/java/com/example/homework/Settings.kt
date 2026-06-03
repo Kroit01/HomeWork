e4626ffdbee48ed62003db7736ab1a76921e0e0d
Kroit <alex.panaetov.96@gmail.com>
2026-06-03 19:54:39 +0400

    commit dev1

---
+++ app/src/main/java/com/example/homework/Settings.kt
@@ -0,0 +1,22 @@
+package com.example.homework
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
+
+        }
+    }
+}
